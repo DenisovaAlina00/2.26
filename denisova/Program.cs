8373b979e0c2f2547e6061ba38221896685895b6
DenisovaAlina00 <113889282+DenisovaAlina00@users.noreply.github.com>
2022-10-03 12:11:49 +0300

    Add files via upload

---
+++ denisova/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class MainClass
+{
+   public static void Main(string[] args)
+    {
+        int n = 564;
+        var p = n % 10;
+        var x = (n - p) / 10 + (p * 100);
+        Console.WriteLine("Трехзначное число равно ={0}", x);
+        Console.ReadKey();
+    }
+
+}
+
